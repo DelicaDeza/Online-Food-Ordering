b5b02473edf7130ea2b0136b7ec8a253ffd0d1d0
Godfireee <godfireee@gmail.com>
2023-05-15 20:27:48 +0530

    logout and session implemented

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,request,jsonify
+from flask import Flask,request,jsonify,session, redirect,url_for
 from models import db,cartitem
 from food_menu import foodmenu
 from login import index
 from forgot import forgot_password
 from signup import create_account
 from datetime import timedelta
+from functools import wraps
 
 def create_app():
     app = Flask(__name__)
@@ -15,12 +16,25 @@ def create_app():
     db.init_app(app)
     return app
 
+
+def login_required(view):
+    @wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if 'email' not in session:
+            return redirect('/login.html')
+        return view(*args, **kwargs)
+    return wrapped_view
+
+
 app = create_app()
 
 @app.route('/food_menu')
+@login_required
 def food():
     return foodmenu()
+
 @app.route("/api/cart", methods=["POST"])
+@login_required
 def add_to_cart():
     data = request.get_json()
     name = data["name"]
@@ -30,15 +44,25 @@ def add_to_cart():
     db.session.add(item)
     db.session.commit()
     return jsonify({"success": True})
+
 @app.route('/create.html', methods=['GET','POST'])
 def signup():
     return create_account(db)
+
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
     return index(app)
+
 @app.route('/forgot.html', methods=['GET', 'POST'])
 def forgot():
     return forgot_password(db)
+
+@app.route('/logout')
+def logout():
+    # Clear the email from the session
+    session.pop('email', None)
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
 
