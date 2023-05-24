8a9e3ab5ba83a3af60f3e2a0d5909af33290d4ef
Harriesh R A <godfireee@gmail.com>
2023-05-24 22:59:49 +0530

    cart and add button tweaks

--- food_menu.py
+++ food_menu.py
@@ -9,20 +9,29 @@ def foodmenu():
     return render_template('food.html', food_items=food_items, canteen=canteen)
 
 
+from sqlalchemy import and_
+
 def add_to_cart(db):
     data = request.get_json()
     name = data["name"]
     netcost = data["netcost"]
-    quantity = data["quantity"]  # Retrieve the quantity from the request data
+    quantity = data["quantity"]
     user_id = data["user_id"]
 
-    if quantity and quantity > 0:  # Check if quantity is not None and greater than 0
-        item = cart(product_name=name, product_cost=netcost, product_quantity=quantity,status_id=3,user_id= user_id)
-        db.session.add(item)
-        db.session.commit()
-        return jsonify({"success": True})
+    if quantity and quantity > 0:
+        existing_item = cart.query.filter(and_(cart.product_name == name, cart.user_id == user_id)).first()
+        if existing_item:
+            existing_item.product_quantity = quantity
+            db.session.commit()
+            return jsonify({"success": True})
+        else:
+            new_item = cart(product_name=name, product_cost=netcost, product_quantity=quantity, status_id=3, user_id=user_id)
+            db.session.add(new_item)
+            db.session.commit()
+            return jsonify({"success": True})
     else:
         return jsonify({"success": False, "error": "Invalid quantity"})
+
 
 
 def update_cart_item_quantity(db):
@@ -30,10 +39,18 @@ def update_cart_item_quantity(db):
     productName = data["productName"]
     quantity = data["quantity"]
 
-    # Find the cart item in the database by product name and update its quantity
-    cart_item = cartitems.query.filter_by(name=productName).first()
+    # Find the cart item in the database by product name
+    cart_item = cart.query.filter_by(product_name=productName).first()
+
     if cart_item:
-        cart_item.quantity = quantity
+        if quantity == 0:
+            # Delete the cart item if the quantity is 0
+            db.session.delete(cart_item)
+        else:
+            # Update the cart item quantity
+            cart_item.product_quantity = quantity
+
         db.session.commit()
 
     return jsonify({"success": True})
+
